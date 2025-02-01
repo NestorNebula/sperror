c53d3ef11fbd98d899a28c13fdf5676efebe65d9
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-01 10:56:02 +0100

    feat: add names to extend classes

--- src/extend.ts
+++ src/extend.ts
@@ -1,20 +1,26 @@
 import Sperror from './sperror';
 
 interface BackendSperror extends Sperror {
+  name: 'BackendSperror';
   statusCode: number;
 }
 
 class BackendSperror extends Sperror {
+  name: 'BackendSperror' = 'BackendSperror';
+
   constructor(title: string, message: string, statusCode: number) {
     super(title, message, statusCode);
   }
 }
 
 interface FrontendSperror extends Sperror {
+  name: 'FrontendSperror';
   statusCode: undefined;
 }
 
 class FrontendSperror extends Sperror {
+  name: 'FrontendSperror' = 'FrontendSperror';
+
   constructor(title: string, message: string) {
     super(title, message);
   }
